fe54d727b6419d31aa087b206dcc0d3cdfc29e5f
alissondiasdev <alissondias120894@gmail.com>
2024-02-13 11:38:56 -0300

    alterando botão de logar

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,16 +1,25 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthGoogleContext } from "../../contexts/authGoogle";
+import GoogleButton from 'react-google-button'
 
-export default function Login  ()  {
-  
+export default function Login() {
+
   const { signInGoogle, signed } = useContext(AuthGoogleContext);
 
   async function handleLoginFromGoogle() {
     await signInGoogle();
   }
   if (!signed) {
-    return <button onClick={handleLoginFromGoogle}>Logar com o Google</button>;
+    return <div>
+      
+      <GoogleButton
+      label="Logar com sua conta Google"
+      onClick={() => { handleLoginFromGoogle() }}
+    />
+      
+    </div> 
+    
   } else {
     return <Navigate to="/Home" />;
   }
